71b68b8312f1f0ff360fdfe948e0da6e5e4de28c
matthewsfrazier <52262095+matthewsfrazier@users.noreply.github.com>
2022-04-25 15:29:58 -0400

    Update streamlit_app.py
    added index on fruit and added multi-select options

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,4 +12,11 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
+
+# picklist so that fruit can be selected that is to be included
+streamlit.multiselect('Pick some fruits:', list(my_fruit_list.index))
+
+#display of fruits table on page
 streamlit.dataframe(my_fruit_list)
+
