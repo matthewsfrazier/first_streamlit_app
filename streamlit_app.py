fb9c45d1fe818deeccc9376615576546001f31ae
matthewsfrazier <52262095+matthewsfrazier@users.noreply.github.com>
2022-04-25 14:56:34 -0400

    Update streamlit_app.py
    Cleaned some carriage returns
    Added import pandas
    imported foreign file adding it to dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 
 streamlit.title('My Mom\'s New Healthy Diner')
+
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
@@ -8,3 +9,7 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+import panadas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+strreamlit.dataframe(my_fruit_list)
