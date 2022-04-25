ac42e4ba5aeef6d3f5106adf04ea51336dd1a973
matthewsfrazier <52262095+matthewsfrazier@users.noreply.github.com>
2022-04-25 14:39:44 -0400

    Update streamlit_app.py
    Changed Title - It  is Mom's idea
    Added Menu Items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
-streamlit.title('My Parents New Healthy Diner')
+streamlit.title('My Mom\'s New Healthy Diner')
+streamlit.header ('Breakfast Menu')
+streamlit.text ('Omega 3 & Blueberry Oatmeal')
+streamlit.text ('Kale, SPinach & Rockeet Smoothie')
+streamlit.text ('Hard-Boiled Egg Free-Rrange Egg')
